8645a9bb4ffce2bec40dd14dea8d74c880db450f
ftoumHub <georgesginon@gmail.com>
2026-03-09 08:39:52 +0100

    with old code versions

--- src/main/java/init/operations/Lab02StreamCollectExample.java
+++ src/main/java/init/operations/Lab02StreamCollectExample.java
@@ -1,8 +1,6 @@
 package init.operations;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("all")
@@ -17,8 +15,28 @@ public class Lab02StreamCollectExample {
         List<String> collectedList = fruits.stream().collect(Collectors.toList());
         // [Pomme, Banane, Orange, Mangue, Pomme]
 
+
+        List<String> collect = new ArrayList<>();
+
+        for (int i = 0; i < fruits.size(); i++) {
+            collect.add(fruits.get(i));
+        }
+        System.out.println(collect);
+
+        List<String> collectList = new ArrayList<>();
+
+        Iterator<String> iterator = fruits.iterator();
+        while (iterator.hasNext()) {
+            collectList.add(iterator.next());
+        }
+        System.out.println(collectList);
+
+
         Set<String> collectedSet = fruits.stream().collect(Collectors.toSet());
         // [Mangue, Banane, Pomme, Orange]
+
+
+
 
         String collectedString = fruits.stream().collect(Collectors.joining(","));
         // Pomme,Banane,Orange,Mangue,Pomme
